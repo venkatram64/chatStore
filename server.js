432d0612f4c577b7e06404c10e6d5057fb6bd59c
venkatram64 <venkat.veerareddy@hotmail.com>
2017-11-07 09:49:49 +0530

    added code

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var express = require('express'),
 app = express(),
 server = require('http').createServer(app),
-io = require('socket.io').listen(server);
+io = require('socket.io').listen(server),
+mongoose = require('mongoose');
 
 var usernames = [];
 server.listen(process.env.PORT || 3000, function(){
 console.log("Server started on port 3000...");
 });
+
+mongoose.connect('mongodb://localhost/chat', function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log('Connected to mongodb...');
+    }
+});
+
+var chatSchema = mongoose.Schema({
+    nick: String,
+    msg: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Chat = mongoose.model('Message',chatSchema);
 
 var path = require('path');
 app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
@@ -18,6 +35,11 @@ res.sendFile(__dirname + '/index.html');
 
 io.sockets.on('connection', function(socket){
 console.log('Socket Connected...');
+var query = Chat.find({});
+query.sort('-created').limit(8).exec(function(err, docs){
+    if(err) throw err;
+    socket.emit('load old msgs', docs);
+});
 
 //new user
 socket.on('new user', function(data, callback){
@@ -36,7 +58,12 @@ function updateUsernames(){
 }
 //send message
 socket.on('send message', function(data){
-    io.sockets.emit('new message', {msg:data, user:socket.username});
+    var newMsg = new Chat({msg:data, nick:socket.username});
+    newMsg.save(function(err){
+        if(err) throw err;
+        io.sockets.emit('new message', {msg:data, nick:socket.username});
+    })
+    
 });
 //Disconnect
 socket.on('disconnect', function(data){
